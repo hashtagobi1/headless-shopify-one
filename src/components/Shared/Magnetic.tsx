ebaece66ba66544e0c3cdf700727dbe5db60e4ff
hashtagobi1 <anokwuruobi@gmail.com>
2023-12-13 03:14:01 +0000

    logic check -- button

--- src/components/Shared/Magnetic.tsx
+++ src/components/Shared/Magnetic.tsx
@@ -22,21 +22,26 @@ const Magnetic = ({ children }: any) => {
       ease: "elastic.out(1,0.3)",
     });
 
-    magnetic!.current!.addEventListener("mousemove", (e: MouseEvent) => {
-      const { clientX, clientY } = e;
-      //   ? returns size of an element and position relative to top part of page
-      const { height, width, left, top } =
-        magnetic!.current!.getBoundingClientRect();
-      const x = clientX - (left + width / 2);
-      const y = clientY - (top + height / 2);
-      xTo(x * 0.35);
-      yTo(y * 0.35);
-    });
-    magnetic!.current!.addEventListener("mouseleave", (e: MouseEvent) => {
-      xTo(0);
-      yTo(0);
-    });
+    if (magnetic.current) {
+      magnetic.current.addEventListener("mousemove", (e: MouseEvent) => {
+        const { clientX, clientY } = e;
+        //   ? returns size of an element and position relative to top part of page
+        if (magnetic.current) {
+          const { height, width, left, top } =
+            magnetic.current.getBoundingClientRect();
+          const x = clientX - (left + width / 2);
+          const y = clientY - (top + height / 2);
+          xTo(x * 0.35);
+          yTo(y * 0.35);
+        }
+      });
+      magnetic.current.addEventListener("mouseleave", (e: MouseEvent) => {
+        xTo(0);
+        yTo(0);
+      });
+    }
   }, []);
+
   return React.cloneElement(children, { ref: magnetic });
 };
 
